6d507de9e93767768ab0bad9ff9fa66eef5c66c3
jijo78 <giuche@gmail.com>
2020-12-06 17:05:17 +0000

    fixed filter width

--- src/Search/Search.test.tsx
+++ src/Search/Search.test.tsx
@@ -1,30 +1,12 @@
 import React from 'react'
-import { render, cleanup, fireEvent } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
-import { cache } from 'swr'
-
-import 'whatwg-fetch'
 
 import { Search } from './Search'
-import { resultsList } from '../fixtures/results-mock-resp'
-const server = setupServer(
-  rest.get(
-    'https://api.themoviedb.org/3/search/multi?api_key=400827fd50b322764fdf4d571822a4a05&append_to_response=credits',
-    (_req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(resultsList))
-    }
-  )
-)
-console.log('server: ', server)
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+
 const renderComponent = () => render(<Search></Search>)
 describe('<Search />', () => {
   afterEach(cleanup)
-  afterEach(() => cache.clear())
 
   it('should render the page', async () => {
     const { findByText } = renderComponent()
