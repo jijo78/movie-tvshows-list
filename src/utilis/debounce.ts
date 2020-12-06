5328b2cf5bac82e5acb9e4109665ec0e34715596
jijo78 <giuche@gmail.com>
2020-12-06 16:49:16 +0000

    added search test

--- src/utilis/debounce.ts
+++
@@ -1,9 +0,0 @@
-export const debounce = (callback, wait) => {
-  let timerId
-  return (...args) => {
-    clearTimeout(timerId)
-    timerId = setTimeout(() => {
-      callback(...args)
-    }, wait)
-  }
-}
